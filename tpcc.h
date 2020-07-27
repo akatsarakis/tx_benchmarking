5371286333735e8fa50f8c42f859b768d40a6e39
jasha64 <17300180070@fudan.edu.cn>
2020-07-28 02:40:58 +0800

    TPC-C schema definition and data population

---
+++ tpcc.h
@@ -0,0 +1,190 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>  // struct tm 
+
+//////////////////
+// Create Schema
+//////////////////
+
+typedef struct
+{
+    int w_id;  // primary key; this field need to hold up to 2*W unique IDs
+    char w_name[11];
+    char w_street_1[21];
+    char w_street_2[21];
+    char w_city[21];  // varchar
+    char w_state[3];
+    char w_zip[10];
+    float w_tax; // numeric(4, 4) signed; sales tax
+    float w_ytd; // numeric(12, 2) signed; Year to date balance
+} __attribute__((packed)) warehouse_t;
+
+// ???
+// Can simulate varchar with char[]? (I know in C++ we can use std::vector<char> but not in C)
+//  also, do we need padding on char array length? i.e., char[17] for varchar(16)
+
+// ???
+// Why do we use __attribute__((packed)), bit field or Neil field? Is it because we have
+//  stringent limit on memory use? (__attribute__((packed)) may in fact slow down the program...)
+// What is a Neil field? No information about Neil field on Google
+
+typedef struct
+{  // 10 districts populated per warehouse
+	int8_t d_id; // 20 unique IDs
+    int d_w_id;  // 2*W unique IDs
+    char d_name[11];
+    char d_street_1[21];
+    char d_street_2[21];
+    char d_city[21];
+    char d_state[3];
+    char d_zip[10];
+    float d_tax;  // numeric(4, 4) signed
+    float d_ytd;  // numeric(12, 2) signed
+    int d_next_o_id;  // Next available Order number
+    // primary key (D_W_ID, D_ID),
+    // foreign key (D_W_ID) references WAREHOUSE(W_ID)
+} __attribute__((packed)) district_t;
+
+typedef struct
+{  // 3000 customers populated per district
+	int c_id;  // 96,000 unique IDs
+    int8_t c_d_id;  // 20 unique IDs
+    int c_w_id;  // 2*W unique IDs
+    char c_first[17];  // varchar(16)
+    char c_middle[3];
+    char c_last[17];
+    char c_street_1[21];
+    char c_street_2[21];
+    char c_city[21];
+    char c_state[3];
+    char c_zip[10];
+    char c_phone[17];
+    struct tm* c_since;
+    char c_credit[3];  // "GC" == good, "BC" == bad
+    float c_credit_lim;  // numeric(12, 2) signed
+    float c_discount;  // numeric(4, 4) signed
+    float c_balance;  // numeric(12, 2) signed
+    float c_ytd_payment;  // numeric(12, 2) signed
+    uint16_t c_payment_cnt;  // numeric(4) unsigned
+    uint16_t c_delivery_cnt;  // numeric(4) unsigned
+    char c_data[501];  // miscellaneous information
+    // primary key (C_W_ID, C_D_ID, C_ID),
+	// foreign key (C_W_ID, C_D_ID) references DISTRICT(D_W_ID, D_ID)
+} __attribute__((packed)) customer_t;
+
+typedef struct
+{
+	int h_c_id; // 96,000 unique IDs
+	int8_t h_c_d_id;  // 20 unique IDs
+	int h_c_w_id;  // 2*W unique IDs
+	int8_t h_d_id;  // 20 unique IDs
+	int h_w_id;  // 2*W unique IDs
+	struct tm* h_date;
+	float h_amount;  // numeric(6, 2)
+	char h_data[25];  // Miscellaneous information
+    // foreign key (H_C_W_ID, H_C_D_ID, H_C_ID) references CUSTOMER(C_W_ID, C_D_ID, C_ID),
+    // foreign key (H_W_ID, H_D_ID) references DISTRICT(D_W_ID, D_ID)
+} __attribute__((packed)) history_t;
+// The TPC-C application does not have to be capable of utilizing the
+//  increased range of C_ID values beyond 6,000.
+
+typedef struct
+{
+	int o_id; // 10,000,000 unique IDs
+	int8_t o_d_id;  // 20 unique IDs
+	int o_w_id;  // 2*W unique IDs
+	int o_c_id;  // 96,000 unique IDs
+    struct tm* o_entry_d;
+	int o_carrier_id; // 10 unique IDs or -1 (denotes null)
+	uint8_t o_ol_cnt; // numeric(2); Count of Order-Lines
+	uint8_t o_all_local; // numeric(1)
+	// primary key (O_W_ID, O_D_ID, O_ID),
+	// foreign key (O_W_ID, O_D_ID, O_C_ID) references CUSTOMER(C_W_ID, C_D_ID, C_ID)
+} __attribute__((packed)) order_t;
+
+typedef struct
+{
+	int no_o_id;  // 10,000,000 unique IDs
+	int8_t no_d_id;  // 20 unique IDs
+	int no_w_id;  // 2*W unique IDs
+    // primary key (NO_W_ID, NO_D_ID, NO_O_ID),
+	// foreign key (NO_W_ID, NO_D_ID, NO_O_ID) references ORDERtable(O_W_ID, O_D_ID, O_ID)
+} __attribute__((packed)) neworder_t;
+
+typedef struct
+{  // 100,000 items are populated (constant)
+	int i_id;  // primary key; 200,000 unique IDs
+	int i_im_id;  // 200,000 unique IDs; Image ID associated to Item
+	char i_name[25];  // varchar(24)
+	float i_price;
+	char i_data[51];  // Brand information
+} __attribute__((packed)) item_t;
+
+typedef struct
+{  // 100,000 populated per warehouse
+	int s_i_id;  // 200,000 unique IDs
+	int s_w_id;  // 2*W unique IDs
+	int s_quantity;  // numeric(4) signed
+	char s_dist[10][25]; // S_DIST_01 char(24), S_DIST_02 char(24), ..., S_DIST_10 char(24)
+	uint32_t s_ytd;  // numeric(8)
+	uint16_t s_order_cnt;  // numeric(4)
+	uint16_t s_remote_cnt;  // numeric(4)
+	char s_data[51];  // Make information
+    // primary key (S_W_ID, S_I_ID),
+	// foreign key (S_W_ID) references WAREHOUSE(W_ID),
+	// foreign key (S_I_ID) references ITEM(I_ID)
+} __attribute__((packed)) stock_t;
+
+typedef struct
+{
+	int ol_o_id;  // 10,000,000 unique IDs
+	int8_t ol_d_id;  // 20 unique IDs
+	int ol_w_id;  // 2*W unique IDs
+	int8_t ol_number;  // 15 unique IDs
+	int ol_i_id;  // 200,000 unique IDs
+	int ol_supply_w_id;  // 2*W unique IDs
+	struct tm* ol_delivery_d;  // datetime or null
+	uint8_t ol_quantity;  // numeric(2)
+	float ol_amount;  // numeric(6, 2) signed
+	char ol_dist_info[25];  // char(24)
+	// primary key (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER),
+	// foreign key (OL_W_ID, OL_D_ID, OL_O_ID) references ORDERtable(O_W_ID, O_D_ID, O_ID),
+	// foreign key (OL_SUPPLY_W_ID, OL_I_ID) references STOCK(S_W_ID, S_I_ID)
+} __attribute__((packed)) orderline_t;
+
+////////////////////////
+// Database Population
+////////////////////////
+
+int Random(int l, int r);
+struct tm cur_local_time();
+void swap(int* a, int* b);
+int nurand(int A, int x, int y);
+void initialize_and_permute_random(int* permutation, int n);
+int gen_rand_astr(char* str, int lenl, int lenh);
+void gen_rand_nstr(char* str, int len);
+void gen_rand_lastname(char* lastname, int x);
+void gen_rand_datafield(char* data);
+void gen_rand_zip(char* zipcode);
+
+void init_db_population(const int n_warehouse);
+
+inline int Random(int l, int r)  // uniform, inclusive
+{
+	// ??? How to implement this function? (See the comments below)
+	if (r - l > RAND_MAX) return rand() / (double)RAND_MAX * (r-l) + l;
+	// Due to the discrete nature of floating-point numbers, this may mean that
+	//  some values will just never show up in your output stream.
+	// In C++11 we can use the std::default_random_engine library to solve this problem,
+	//  but not in C
+    else return rand() % (r-l+1) + l;
+	// So I use another way (as above) when r - l <= RAND_MAX, but this is also problematic 
+	//  because the result is not uniformly distributed when RAND_MAX % (r-l+1) != 0.
+	// Maybe we shall choose "the lesser of two evils"
+}
+inline struct tm cur_local_time()
+{
+	time_t cur_time = time(NULL);
+	return *localtime(&cur_time);
+}
+inline void swap(int* a, int* b) { int t = *a; *a = *b; *b = t; }
